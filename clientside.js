129624e1b11b17b8995a2c751bbe759a537152fa
Ana995 <anita_new7@yahoo.com>
2016-06-14 18:13:34 +0300

    test

--- clientside.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by V3790154 on 5/31/2016.
- */
-$('#test').click(function() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:3333'
-    });
-});
--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -3,33 +3,5 @@
  */
 //module
 var weatherApp = angular.module('weatherApp',['ngRoute','ngResource']);
-//routes
-console.log("blabla");
 
-weatherApp.config( '$routeProvider',function($routeProvider){
-
-    $routeProvider
-
-        .when('/',{
-            templateUrl:'pages/home.htm',
-            controller:'homeCtrl'
-        })
-       .when('/forecast',{
-            templateUrl:'pages/forecast.htm',
-            controller:'forecastCtrl'
-    })
-        .otherwise({
-            redirectTo: 'angular/index.html'
-        });
-    console.log("Alert!!!");
-});
-
-//controllers
-weatherApp.controller('homeCtrl',['$scope',function($scope){
-    console.log("Alert!!");
-
-}]);
-
-weatherApp.controller('forecastCtrl',['$scope',function($scope){
-
-}]);
+var myApp=angular.module('myapp',[]);
