a3a1592a5c8f6cb33f127e9aef0e5b14cafe302f
Ana995 <anita_new7@yahoo.com>
2016-06-27 17:59:22 +0300

    test

---
+++ calendar/server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+const PORT = 9998;
+//var nano = require('nano')('http://localhost:5984');
+//var db_name = "books";
+
+
+app.use(express.static('./'));
+app.use
+
+/*app.get("/product",function( req,res){
+   var products="getproduct.js";
+});*/
+app.get("/", function (req, res) {
+     res.sendFile(__dirname + "/" + "index.html");
+     /*req.insert_doc= function(doc, tried) {
+         db.insert(doc,
+             function (error, http_body, http_headers) {
+                 if (error) {
+                     if (error.message === 'no_db_file' && tried < 1) {
+                         // create database
+                         return nano.db.create(db_name, function () {
+                             insert_doc(doc, tried + 1);
+                         });
+                     }
+                     else {
+                         return console.log(error);
+                     }
+                 }
+                 console.log(http_body);
+             });
+     }*/
+    });
+
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
